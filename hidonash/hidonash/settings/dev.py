a78b640f93c2e56b21fc5d68911ca7ed0398b46e
SimonZimmer <simon@dear-reality.com>
2023-07-14 16:27:39 +0200

    add base project

---
+++ hidonash/hidonash/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-em&49ag&4nakx$*$mav!ozv+vdp!)f_1$jrc+ly%-j-96-m#q7"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
