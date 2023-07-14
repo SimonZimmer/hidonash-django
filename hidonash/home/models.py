8a26d3f8390e034b9af7e3dacaaa80d8f89a212e
SimonZimmer <simon@dear-reality.com>
2023-07-14 18:07:07 +0200

    add image upload

--- hidonash/home/models.py
+++ hidonash/home/models.py
@@ -7,7 +7,20 @@ from wagtail.admin.panels import FieldPanel
 
 class Epk(Page):
     bio = RichTextField(blank=True)
+    image = models.ForeignKey(
+            "wagtailimages.Image",
+            null=True,
+            blank=False,
+            on_delete=models.SET_NULL,
+            related_name="+",
+        )
 
     content_panels = Page.content_panels + [
         FieldPanel('bio'),
+        FieldPanel('image')
     ]
+
+    @property
+    def rendition_url(self):
+        url = self.image.get_rendition('fill-800x800|jpegquality-60').url
+        return url
