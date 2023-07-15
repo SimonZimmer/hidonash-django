3ad9745f04973f11ac8652cf88014d25e04fa953
SimonZimmer <simon@dear-reality.com>
2023-07-15 16:19:44 +0200

    add remaining items

--- hidonash/home/models.py
+++ hidonash/home/models.py
@@ -6,6 +6,7 @@ from wagtail.admin.panels import FieldPanel
 
 
 class Epk(Page):
+    quote = RichTextField(blank=True)
     bio = RichTextField(blank=True)
     image = models.ForeignKey(
             "wagtailimages.Image",
@@ -14,13 +15,28 @@ class Epk(Page):
             on_delete=models.SET_NULL,
             related_name="+",
         )
+    spotlight_track_1 = models.URLField(blank=True)
+    spotlight_track_2 = models.URLField(blank=True)
+    spotlight_track_3 = models.URLField(blank=True)
+
+    spotlight_mix_1 = models.URLField(blank=True)
+    spotlight_mix_2 = models.URLField(blank=True)
+
+    video = models.URLField(blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel('quote'),
         FieldPanel('bio'),
-        FieldPanel('image')
+        FieldPanel('image'),
+        FieldPanel('spotlight_track_1'),
+        FieldPanel('spotlight_track_2'),
+        FieldPanel('spotlight_track_3'),
+        FieldPanel('spotlight_mix_1'),
+        FieldPanel('spotlight_mix_2'),
+        FieldPanel('video')
     ]
 
     @property
     def rendition_url(self):
-        url = self.image.get_rendition('fill-800x800|jpegquality-60').url
+        url = self.image.get_rendition('fill-800x500|jpegquality-60').url
         return url
